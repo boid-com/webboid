b78e6adf0af31b1431a366edd466820fb867849c
John Heeter <john@boid.com>
2018-01-08 22:48:58 -0600

    ssl?

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var history = require('connect-history-api-fallback')
-
+var sslRedirect = require('heroku-ssl-redirect');
+var port = process.env.PORT || 8080
 var app = express()
 
 app.use(history())
-
+app.use(sslRedirect())
 app.use(express.static('./dist'))
 
-var port = process.env.PORT || 8080
 app.listen(port, (event) => {
   console.log(event)
 })
