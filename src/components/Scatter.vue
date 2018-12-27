f8435be13d76a3dc1d4ffd31a634055b15592d0b
NatPDeveloper <natanael.prudhomme@gmail.com>
2018-12-27 15:52:24 -0500

    auth by only user fix

--- src/components/Scatter.vue
+++ src/components/Scatter.vue
@@ -8,6 +8,8 @@ section.example
       br
       section(v-if='scatter && account')
         button(@click='stake') Stake
+        input(v-model.number='stakeAmount', type='number', placeholder='stake amount')
+        input(v-model.number='stakeTime', type='number', placeholder='stake time')
       section(v-if='scatter && account')
         button(@click='unstake') Unstake
       section(v-if='scatter && account')
@@ -25,7 +27,7 @@ section.example
     // Importing eosjs / Scatter
     import ScatterJS, {Network} from 'scatterjs-core';
     import ScatterEOS from 'scatterjs-plugin-eosjs2';
-    import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'eosjs';
+    import { Api, JsonRpc } from 'eosjs';
 
     // Adding ScatterJS plugins
     ScatterJS.plugins( new ScatterEOS() );
@@ -52,13 +54,16 @@ section.example
     const rpc = new JsonRpc(network.fullhost());
     export default {
         data(){return {
-            scatter:null
+            scatter:null,
+            stakeAmount:null,
+            stakeTime:null
         }},
         mounted(){
             this.setEosInstance();
             ScatterJS.scatter.connect('boid').then(connected => {
                 if(!connected){
                     console.error('Could not connect to Scatter.');
+                    alert('Could not connect to Scatter, try refreshing the page');
                     return;
                 }
                 this.scatter = ScatterJS.scatter;
@@ -89,8 +94,8 @@ section.example
                         }],
                         data: {
                             _stake_account: this.account.name,
-                            _stake_period: '1',
-                            _staked: '500.0000 BOID',
+                            _stake_period: this.stakeTime,
+                            _staked: this.stakeAmount.toFixed(4) + ' BOID',
                             memo: 'memo'
                         },
                     }]
@@ -100,23 +105,27 @@ section.example
                 });
             },
             async unstake(){
-                const options = { authorization:[`${this.account.name}@${this.account.authority}`] };
-                await eos.transact({
-                        actions: [{
-                            account: 'we3i5kdsdo12',
-                            name: 'unstake',
-                            authorization: [{
-                                actor: this.account.name,
-                                permission: this.account.authority,
-                            }],
-                            data: {
-                                _stake_account: this.account.name
-                            },
-                        }]
-                    }, {
-                        blocksBehind: 3,
-                        expireSeconds: 30,
-                    });
+                try {
+                    const options = { authorization:[`${this.account.name}@${this.account.authority}`] };
+                    await eos.transact({
+                            actions: [{
+                                account: 'we3i5kdsdo12',
+                                name: 'unstake',
+                                authorization: [{
+                                    actor: this.account.name,
+                                    permission: this.account.authority,
+                                }],
+                                data: {
+                                    _stake_account: this.account.name
+                                },
+                            }]
+                        }, {
+                            blocksBehind: 3,
+                            expireSeconds: 30,
+                        });
+                } catch (e) {
+                    console.log(e);
+                }
             },
             async claim(){
                 const options = { authorization:[`${this.account.name}@${this.account.authority}`] };
@@ -128,15 +137,14 @@ section.example
                             actor: this.account.name,
                             permission: this.account.authority,
                         }],
-                        data: {
-                            _stake_account: this.account.name
-                        },
+                    data: {
+                        _stake_account: this.account.name
+                    },
                     }]
                 }, {
                     blocksBehind: 3,
                     expireSeconds: 30,
                 });
-                    
             },
             setEosInstance(){
                 if(this.account){
