72c2142bdc5fcacc83e59f86ab426ab0cb8e02c8
John Heeter <john@Boid.com>
2019-05-14 20:52:56 -0500

    progress

--- src/components/Device.vue
+++ src/components/Device.vue
@@ -1,22 +1,7 @@
 <template lang="pug">
 div(style="height:100%; overflow:hidden;")
-  div(v-if="!loading && thisDevice.name").bg-grey-3
-    .row.justify-center(style="height:45px; overflow:hidden")
-      q-icon(:name="thisDevice.icon")
-      div( v-if="!editName" style="padding:13px;").text-black {{thisDevice.name}}
-      div(v-else)
-        q-input( @keyup.enter="editDeviceName(newDeviceName)" style="width:210px; height:10px; padding:5px; margin:5px; padding-bottom:15px; " placeholder="new device name" v-model="newDeviceName")
-      div(v-if="!editName")
-        q-btn.infobtn(small round flat @click="editName = true" style="height:25px; width:30px; margin-top:3px" )
-          q-icon(name="edit" size="20px")
-          q-tooltip Edit device name
-      div(v-else).on-right
-        q-btn.infobtn(small round color="green" @click="editDeviceName(newDeviceName)" style="height:25px; width:30px; margin-top:0px")
-          q-icon(name="check" color="white" size="20px")
-          q-tooltip Confirm name edit
-        q-btn.infobtn.on-right(small round flat color="amber" @click="newDeviceName = '', editName = false" style="height:25px; width:30px; margin-top:5px")
-          q-icon(name="close" color="red-5" size="20px")
-          q-tooltip Cancel name edit
+  div(v-if="!loading && thisDevice.name" ref="navigation").bg-grey-3
+    deviceName(:thisDevice="thisDevice")
     .row.justify-center.no-wrap
       div(v-for="nav in navigation")
         q-btn( flat :class="{selected:page===nav.name}" @click="page = nav.name" :disable="nav.disabled")
@@ -26,15 +11,11 @@ div(style="height:100%; overflow:hidden;")
             q-tooltip Coming soon
   .row.justify-center.relative-position(v-if="!loading && thisDevice.name" style="padding:5px;")
     .full-width
-      cpuWidget(v-show="page === 'CPU' || page === 'Dash' "
-        :selected="page === 'CPU'" :activeTasks="activeTasks"
-        :boincStatusIcon="boincStatusIcon" :onBatteries="onBatteries" :boincStatus="boincStatus" 
-        @deselected="page = 'Dash'" @openConfigModal="openConfigModal" @selected="page = 'CPU'" @toggle="toggleCPU")
+      //- cpuWidget(v-show="page === 'CPU' || page === 'Dash'"
+        //- :thisDevice="thisDevice" ref="cpuWidget")
     .full-width
       gpuWidget(v-show="page === 'GPU' || page === 'Dash' "
-        :thisDevice="thisDevice"
-        ref="gpuWidget"
-        )
+        :thisDevice="thisDevice" ref="gpuWidget")
     .full-width.relative-position
       hddWidget(:disabled="true" v-show="page === 'HDD' || page === 'Dash' " :thisDevice="thisDevice")
   .layout-padding.relative-position(v-else)
@@ -48,10 +29,7 @@ div(style="height:100%; overflow:hidden;")
   //- div(style="height:20px").full-width.bg-grey-8.absolute-bottom
   q-modal(ref="modal" @close="thisModal=null, modalData=null" )
     component(:is="thisModal" :thisModal="$refs.modal" :data="modalData" v-if="thisModal")
-  textarea(v-show="false" ref="copyText" value="")
-
-
-  </template>
+</template>
 
 <script>
 import parseDevice from 'src/lib/parseDevice'
@@ -60,6 +38,8 @@ import cpuWidget from '@/cpuWidget.vue'
 import gpuWidget from '@/gpuWidget.vue'
 import hddWidget from '@/hddWidget.vue'
 import gpuConfig from '@/gpuConfig.vue'
+import deviceName from '@/deviceName.vue'
+
 var masterInterval = null
 function ec(err){
   console.error(err)
@@ -81,9 +61,10 @@ function setupDevice(device) {
 }
 
 export default {
-  components:{cpuWidget,gpuWidget,gpuConfig,hddWidget},
+  components:{cpuWidget,gpuWidget,gpuConfig,hddWidget,deviceName},
   data() {
     return {
+      thisDevice:null,
       editName:false,
       newDeviceName:"",
       thisModal:null,
@@ -106,29 +87,14 @@ export default {
       }],
       page:"Dash",
       ipcRenderer:null,
-      loading: true,
-      initialized:false,
+      loading: false,
+      initialized:true,
       parseDevice,
     }
   },
   computed: {},
   methods: {
-    openURL,
-    editDeviceName: async function(name){
-      try {
-        this.newDeviceName = ""
-        this.editName = false
-        this.thisDevice.name = name
-        const result = await this.$api.modifyDevice({id:this.thisDevice.id,newName:name})
-        console.log(result)
-        if (!result) alert('problem updating device name')
-        if (result.error) alert(result.error)
-        this.thisDevice = await this.$api.getDevice({id:this.thisDevice.id}).catch(ec)
-      }
-      catch(error){
-        alert(error)
-      }
-    },
+    openURL
   },
   props: ['thisUser', 'authenticated', 'api', 'ch'],
   mounted() {
