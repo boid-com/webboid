d159531981a19c6e9dd62a82d94509ffe183bbc2
John Heeter <john@Boid.com>
2019-10-08 17:23:47 -0500

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,15 @@
 div(style="min-height:100px; margin-top:40px;").full-width.bg-grey-9
   .row.justify-end
     .col-auto(style="margin: 10px;")
+      q-btn.on-right(flat @click="openURL('https://community.boid.com')")
+        q-icon(name="fa-users")
+      q-btn.on-right(flat @click="openURL('https://t.me/Boidcom_official')")
+        q-icon(name="fa-telegram")
       q-btn(flat @click="openURL('https://discordapp.com/invite/zJnRpVz')")
-        //- q-icon(name="fa-discord")
         img(src="/statics/images/discordicon.svg")
       q-btn.on-right(flat @click="openURL('https://twitter.com/boidcom')")
-        //- img(src="/statics/images/twitterlogo.svg")
         q-icon(name="fa-twitter")
-
-      q-btn.on-right(flat @click="openURL('https://github.com/Boid-John')")
+      q-btn.on-right(flat @click="openURL('https://github.com/boid-com')")
         q-icon(name="fa-github")
       q-btn.text-blue-5(flat @click="openURL('https://www.boid.com')") What is Boid?
       
