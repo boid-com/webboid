66125d81bb73712e1bbf0cba3ec9e073a866a5f2
John Heeter <john@boid.com>
2020-02-17 13:43:28 -0600

    fixed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,17 +2,17 @@
 div(style="min-height:100px; margin-top:40px;").full-width.bg-grey-9
   .row.justify-end
     .col-auto(style="margin: 10px;")
-      q-btn.on-right(flat @click="openURL('https://community.boid.com')")
+      q-btn.on-right(flat @click="openURL('https://community.boid.com')").linkbtn
         q-icon(name="fa-users")
-      q-btn.on-right(flat @click="openURL('https://t.me/Boidcom_official')")
+      q-btn.on-right(flat @click="openURL('https://t.me/Boidcom_official')").linkbtn
         q-icon(name="fa-telegram")
-      q-btn(flat @click="openURL('https://discordapp.com/invite/zJnRpVz')")
+      q-btn.on-right(flat @click="openURL('https://discordapp.com/invite/zJnRpVz')").linkbtn
         img(src="/statics/images/discordicon.svg")
-      q-btn.on-right(flat @click="openURL('https://twitter.com/boidcom')")
+      q-btn.on-right(flat @click="openURL('https://twitter.com/boidcom')").linkbtn
         q-icon(name="fa-twitter")
-      q-btn.on-right(flat @click="openURL('https://discussions.app/tag/boid')")
-        q-icon(src="/statics/images/discussions.svg")
-      q-btn.on-right(flat @click="openURL('https://github.com/boid-com')")
+      q-btn.on-right(flat @click="openURL('https://discussions.app/tag/boid')").linkbtn
+        img(src="/statics/images/discussions.svg")
+      q-btn.on-right(flat @click="openURL('https://github.com/boid-com')").linkbtn
         q-icon(name="fa-github")
       q-btn.text-blue-5(flat @click="openURL('https://www.boid.com')") What is Boid?
       
@@ -21,6 +21,8 @@ div(style="min-height:100px; margin-top:40px;").full-width.bg-grey-9
 .q-btn
   color white
 img
+  width 30px
+.linkbtn
   width 30px
 </style>
 
