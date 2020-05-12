17ddf674ea20944b60e3f8e842ee73ac85f9d90e
John Heeter <john@boid.com>
2020-05-11 22:03:51 -0500

    update IBC instructions

--- src/components/SeasonPass-Pass.vue
+++ src/components/SeasonPass-Pass.vue
@@ -3,11 +3,11 @@ div
   .row.justify-center.relative-position(style="margin-top:0px;")
     q-card.col.no-margin(style="margin-bottom:5px;")
       .row
-        .col
+        .col-xs-12.col-sm-4
           div(style="margin-left:20px;")
             h6 Drawing Prizes
-            .row.justify-center
-              .col-auto
+            .row
+              .col-12
                 .infobox
                   h6 1st
                   ul
@@ -15,7 +15,7 @@ div
                     li + 1,000,000 KROWN
                     li + 500,000 BOID
                     li + 7,000 BOS
-              .col-auto
+              .col-12
                 .infobox
                   h6 2nd
                   ul
@@ -23,7 +23,7 @@ div
                     li + 500,000 KROWN
                     li + 250,000 BOID
                     li + 3500 BOS
-              .col-auto
+              .col-12
                 .infobox
                   h6 3rd
                   ul
@@ -31,6 +31,18 @@ div
                     li + 250,000 KROWN
                     li + 125,000 BOID
                     li + 1,700 BOS
+        .col-xs-12.col-sm-6
+          div(style="margin-left:20px;")
+            h5 IBC token instructions
+            p First you need to transfer a native token from another chain to your EOS account.
+            h6.no-margin Examples
+            pre(style="margin-bottom:10px; margin-top:0px;") {{ibcInstructions}}
+            a(href="https://medium.com/boscore/experience-ibc-protocol-for-eosio-chains-1fd17cbb9302") learn more about IBC
+            div(style="margin:20px;")
+            p After you have received the IBC token on EOS, you can send the token to the 'boiddonation' contract from any wallet or use Scatter + this page to make the transfer.
+            p Remember that IBC transfers have a small fee and the donations contract enforces a minimum donation per token.
+
+
     
 </template>
 <style lang="stylus" scoped>
@@ -52,15 +64,28 @@ div
 
 <script>
 import seasonPass from '../lib/seasonPass'
+const ibcInstructions = `
+TELOS --> EOS
+send tokens to bosibc.io with a memo:
+hub.io@bos >> YOUREOSACCOUNTNAME@eos
+
+BOS --> EOS
+send tokens to bosibc.io with a memo:
+YOUREOSACCOUNTNAME@eos
+
+WAX --> EOS
+send tokens to bosibc.io with a memo:
+hub.io@bos >> YOUREOSACCOUNTNAME@eos
+`
 
 export default {
   data(){
-    return seasonPass.state
+    return Object.assign({ibcInstructions},seasonPass.state)
   },
   methods:seasonPass.methods,
   computed:seasonPass.computed,
   mounted(){
-    
+    // this.ibcInstructions = instructions
   },
   watch:{
     
