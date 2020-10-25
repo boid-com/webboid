f536266b6c205fd2bd4a77697d946c6e1d038eb9
boid <boid@boids-MBP.attlocal.net>
2020-10-25 07:51:45 -0500

    latest

--- src/components/SeasonPass-Select-btn.vue
+++ src/components/SeasonPass-Select-btn.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div.cursor-pointer.relative-position.shadow-2(v-ripple style="margin:10px; padding:0px; width:130px; height:140px; zoom: 1; border-radius:30px; user-select:none;" :class="{selectedpay:selectedPay === coin.symbol}" @click="updateSelectedPay(coin.symbol)")
+div.cursor-pointer.relative-position.shadow-2(v-ripple style="margin:10px; padding:0px; width:130px; height:150px; zoom: 1; border-radius:30px; user-select:none;" :class="{selectedpay:selectedPay === coin.symbol}" @click="updateSelectedPay(coin.symbol)")
   .row.justify-center.center-items(style="width:100%; padding-top:3px;").no-wrap
     .col-auto(style="height:50px;")
       img(:src="coin.img" style="height:100%; padding:10px;")
@@ -10,16 +10,16 @@ div.cursor-pointer.relative-position.shadow-2(v-ripple style="margin:10px; paddi
       p.no-margin {{coin.minContribution}} 
   .row.justify-center.no-wrap(style="margin-bottom:0px;")
     small minimum
-
-      //- small.text-black.light-paragraph Multiplier
   .row.justify-center(style="padding-top:10px").items-center
     .col-auto.center-items(style="margin-right:5px;")
       img(src="/statics/images/BoidPower.svg" style="width:15px; margin:0px;")
     .col-auto.text-green.strong
       p.no-margin + {{calcPowerBonus(parseFloat(coin.availablePower),coin.power_reward_multiplier).toFixed(0)}}
   .row.justify-center
-    div(style="width:80px;").bg-grey-4
-      q-progress(:percentage="reservior" style="height:5px;" color="green-6")
+    div(style="width:80px;")
+      div(style="padding:1px;")
+      q-progress(:percentage="reservior" style="height:5px;" color="amber-6") 
+      q-tooltip Charge: {{reservior}}%
 
 </template>
 <style lang="stylus" scoped>
@@ -33,8 +33,8 @@ div.cursor-pointer.relative-position.shadow-2(v-ripple style="margin:10px; paddi
   .selectedpay
     // background-color $blue-1
     // color white
-    // outline 1px solid $green-1
-    box-shadow: 0 4px 10px 2px $blue-4
+    // outline 6px solid $green-1
+    box-shadow: 0 6px 8px 2px $blue-4
 </style>
 
 <script>
@@ -52,7 +52,7 @@ export default {
   computed:{
     reservior(){
       if(!this.coin)return 0
-      return convertRange(this.coin.availablePower,[0,this.coin.powerCap],[0,100])
+      return convertRange(this.coin.availablePower,[0,this.coin.powerCap],[0,100]).toFixed(0)
     },
     ...seasonPass.computed
   },
