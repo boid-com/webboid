72f845a4c0c03376671846bd9923d1c073019136
boid <boid@boids-MBP.attlocal.net>
2020-10-26 09:44:10 -0500

    latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    }
   },
   env: {
     browser: true
